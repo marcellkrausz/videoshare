400b5336f052dbff0eac140a7d81111cfe7dda40
marcellkrausz <krauszmaarcell@gmail.com>
2021-08-28 22:44:18 +0200

    Initial commit

---
+++ src/main/java/com/marcellk/videoshare/VideoshareApplication.java
@@ -0,0 +1,13 @@
+package com.marcellk.videoshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoshareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VideoshareApplication.class, args);
+    }
+
+}
